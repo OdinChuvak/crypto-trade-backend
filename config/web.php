4847d75d6a5f695103ec4c941477af60632373fb
Timur Makhmudov <buttalbakl@gmail.com>
2021-10-24 03:53:53 +0300

    Добавил запись parsers => ['application/json' => 'yii/web/JsonParser'] для компонента request, чтобы принимать данные в формате json

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'NZgiuMSrUC2Wptnl8x7TZN8dT2KSpDrO',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -47,6 +50,15 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => [
+                        'trading-grid',
+                        'currency-pair',
+                        'order',
+                    ],
+                    'pluralize' => false
+                ],
             ],
         ],
     ],
