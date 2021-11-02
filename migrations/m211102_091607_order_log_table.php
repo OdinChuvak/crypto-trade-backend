77311fe708cdd3effb861d7a8409ed8d09566227
Timur Makhmudov <buttalbakl@gmail.com>
2021-11-02 12:20:30 +0300

    Добавил миграцию таблицы order_log

---
+++ migrations/m211102_091607_order_log_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211102_091607_order_log_table
+ */
+class m211102_091607_order_log_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('order_log', [
+            'id' => $this->primaryKey(),
+            'order_id' => $this->integer()->notNull(),
+            'type' => $this->string(24)->notNull(),
+            'message' => $this->string(512)->notNull(),
+            'created_at' => $this->timestamp()->notNull()->append('DEFAULT CURRENT_TIMESTAMP()'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('order_log');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m211102_091607_order_log_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
