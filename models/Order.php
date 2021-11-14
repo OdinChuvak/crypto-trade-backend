bd18bfda818fd0f25bf0b7e45517c88ebcbc99e0
Timur Makhmudov <buttalbakl@gmail.com>
2021-11-14 15:34:35 +0300

    Добавил комментарии к методу getQuantity

--- models/Order.php
+++ models/Order.php
@@ -4,9 +4,8 @@ namespace app\models;
 
 use app\helpers\AppError;
 use app\helpers\FunctionBox;
-use yii\db\ActiveRecord;
 
-class Order extends ActiveRecord
+class Order extends BaseModel
 {
     public static function tableName()
     {
@@ -17,7 +16,14 @@ class Order extends ActiveRecord
     {
         return [
             ['user_id', 'default', 'value' => FunctionBox::getIdentityId()],
-            ['exmo_order_id', 'safe'],
+            [
+                [
+                    'id',
+                    'exmo_order_id',
+                    'previous_order_id',
+                ],
+                'safe'
+            ],
             [
                 [
                     'trading_grid_id',
@@ -72,48 +78,106 @@ class Order extends ActiveRecord
         $pair = CurrencyPair::findOne(['id' => $grid->pair_id]);
         $actualQuantity = round($grid->order_amount / $this->required_trading_rate, 6);
 
-        if (!($actualQuantity >= $pair->min_quantity && $actualQuantity <= $pair->max_quantity)) {
-            $errorMsg = 'Incorrect value of the quantity of purchased currency. Valid values ​​are from '.$pair->min_quantity.' to '.$pair->max_quantity.'. Please change the currency rate or amount values to adjust the quantity.';
+        if ($actualQuantity < $pair->min_quantity || $actualQuantity > $pair->max_quantity) {
+            $errorMsg = 'Incorrect value of the quantity of purchased currency. Valid values are from '.$pair->min_quantity.' to '.$pair->max_quantity.'. Please change the currency rate or amount values to adjust the quantity.';
             $this->addError('required_trading_rate', $errorMsg);
         }
     }
 
+    /*
+     * Метод возвращает точное количество закупаемой валюты по ордеру,
+     * рассчитывая все в соответствии с типом сетки
+     */
     public static function getQuantity($order_id)
     {
+        /*
+         * Берем данные текущего ордера
+         */
         $order = self::find()
             ->with('grid')
             ->where(['id' => $order_id])
             ->one();
 
+        /*
+         * Берем данные предыдущего ордера
+         */
         $previousOrder = self::find()
             ->with('grid')
             ->where(['id' => $order->previous_order_id])
             ->one();
 
+        /*
+         * Данные сетки и валютной пары пишем в отдельные переменные
+         * По валютной паре будем смотреть точность для курсов и общих сумм `price_precision`
+         */
         $grid = $order->grid;
+        $pair = CurrencyPair::findOne(['id' => $grid->pair_id]);
 
+        /*
+         * Если предыдущего ордера не существует,
+         * количество вернем по настройке сетки.
+         * А именно - разделим order_amount, заданный в сетке,
+         * на необходимый курс для исполнения текущего ордера `required_trading_rate`
+         */
         if (empty($previousOrder)) {
-            return round($grid->order_amount / $order->required_trading_rate, 7);
+            return round($grid->order_amount / $order->required_trading_rate, $pair->price_precision);
         }
 
+        /*
+         * Если предыдущий ордер есть, при этом операции у них равны,
+         * значит, что-то пошло не так. Вернем null,
+         * чтобы вызвать ошибку запроса
+         */
         if ($order->operation === $previousOrder->operation) {
             return null;
         }
 
+        /*
+         * Вывод количество для типа #1 - "торговать на все"
+         * В этом случае:
+         *  - если операция текущего ордера 'buy',
+         * то разделим всю полученную в предыдущем ордере сумму (received),
+         * на необходимый курс для исполнения текущего ордера `required_trading_rate`,
+         *  - если же операция ордера 'sell',
+         * то есть мы собираемся продавать, вернем всю сумму,
+         * полученную в предыдущем ордере (received).
+         */
         if ($grid->trading_method === 1) {
             return $order->operation === 'buy'
-                ? round($previousOrder->received / $order->required_trading_rate, 7)
+                ? round($previousOrder->received / $order->required_trading_rate, $pair->price_precision)
                 : $previousOrder->received;
         }
+        /*
+         * Вывод количество для типа #2 - "Сохранять валюту, на которую покупаем"
+         * В этом случае:
+         *  - если операция текущего ордера 'buy',
+         * то есть мы собираемся покупать,
+         * разделим сумму покупки, заданную в сетке (order_amount),
+         * на необходимый курс для исполнения текущего ордера `required_trading_rate`,
+         *  - если же операция ордера 'sell',
+         * то есть мы собираемся продавать, вернем всю сумму,
+         * полученную в предыдущем ордере (received).
+         */
         elseif ($grid->trading_method === 2) {
             return $order->operation === 'buy'
-                ? round($grid->order_amount / $order->required_trading_rate, 7)
+                ? round($grid->order_amount / $order->required_trading_rate, $pair->price_precision)
                 : $previousOrder->received;
         }
+        /*
+         * Вывод количество для типа #3 - "Сохранять покупаемую валюту"
+         * В этом случае:
+         *  - если операция текущего ордера 'buy',
+         * то есть мы собираемся покупать,
+         * разделим сумму, полученную в предыдущем ордере (received),
+         * на необходимый курс для исполнения текущего ордера `required_trading_rate`,
+         *  - если же операция ордера 'sell',
+         * то есть мы собираемся продавать, вернем сумму заданную в сетке (order_amount),
+         * на необходимый курс предыдущего ордера (required_trading_rate).
+         */
         elseif ($grid->trading_method === 3) {
             return $order->operation === 'buy'
-                ? round($previousOrder->received / $order->required_trading_rate, 7)
-                : round($grid->order_amount / $previousOrder->required_trading_rate, 7);
+                ? round($previousOrder->received / $order->required_trading_rate, $pair->price_precision)
+                : round($grid->order_amount / $previousOrder->required_trading_rate, $pair->price_precision);
         }
         else {
             return null;
