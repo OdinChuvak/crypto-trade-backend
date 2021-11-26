c80d118251d758f9be9a2f0ad6ab253a89c20878
Timur Makhmudov <buttalbakl@gmail.com>
2021-11-26 21:07:26 +0300

    Переопределил метод add() в BaseModel

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -6,10 +6,23 @@ use yii\db\ActiveRecord;
 
 abstract class BaseModel extends ActiveRecord
 {
+    /**
+     * Метод пытается добавить запись в таблице БД
+     * В случае успеха вернет записанную модель,
+     * а в случае ошибки, ту же модель, но содержащую
+     * данные ошибки
+     *
+     * @param $data
+     * @param string $formName
+     * @return static
+     */
     public static function add($data, $formName = '')
     {
         $model = new static();
 
-        return ($model->load($data, $formName) && $model->save());
+        $model->load($data, $formName);
+        $model->save();
+
+        return $model;
     }
 }
