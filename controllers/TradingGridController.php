b5a6420c7243c9e0b9ce21b8d532f67ed66203eb
Timur Makhmudov <buttalbakl@gmail.com>
2021-11-26 23:08:32 +0300

    Удалил переопределенный метод создания записи в controllers/TradingGridController.php

--- controllers/TradingGridController.php
+++ controllers/TradingGridController.php
@@ -2,22 +2,17 @@
 
 namespace app\controllers;
 
-use app\actions\createTradingGridAction;
 use app\actions\IndexAction;
-use app\models\Order;
 use app\models\TradingGrid;
 
 class TradingGridController extends BaseApiController
 {
     public $modelClass = TradingGrid::class;
-    public $orderModelClass = Order::class;
 
     public function actions()
     {
         $actions = parent::actions();
-
         $actions['index']['class'] = IndexAction::class;
-        $actions['create']['class'] = createTradingGridAction::class;
 
         return $actions;
     }
