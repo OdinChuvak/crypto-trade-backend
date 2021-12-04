e11f9fda686240b27f5f30522ba7f3b22e80e84b
Махмудов Тимур Саидович <mahmudov@lbr.ru>
2021-12-04 10:47:03 +0300

    Fix bug in repository Order

--- repositories/Order.php
+++ repositories/Order.php
@@ -26,7 +26,7 @@ class Order
         /**
          * В случае ошибки $exmo = AppError::NO_AUTH_KEY_FILE
          */
-        if (gettype($this->exmo) === 'array') {
+        if (isset($this->exmo['type']) && $this->exmo['type'] === 'error') {
 
             UserLog::add([
                 'user_id' => $user_id,
