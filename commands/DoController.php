5acebf129a7a9ac18fcb9e544bdb4e0e556cf7f4
Махмудов Тимур Саидович <mahmudov@lbr.ru>
2022-01-03 18:31:44 +0300

    Добавил вспомогательный контроллер и экшн, задающий пароль пользователю по email

---
+++ commands/DoController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\commands;
+
+use app\models\User;
+use yii\base\UserException;
+use yii\console\Controller;
+use yii\db\Exception;
+use yii\web\NotFoundHttpException;
+
+class DoController extends Controller
+{
+    /**
+     * @param $email
+     * @param $password
+     *
+     * @throws NotFoundHttpException
+     */
+    public function actionSetUserPassword($email, $password)
+    {
+        $user = User::findOne(['email' => $email]);
+
+        if (!$user) throw new UserException('Пользователь не найден!');
+
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+        $user->password_hash = $password_hash;
+
+        if ($user->save()) {
+            return true;
+        } else {
+            throw new Exception('Ошибка записи данных в БД');
+        }
+    }
+}
