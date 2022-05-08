09c00559519e98c558b3db549794d10f80d1a098
Timur Makhmudov <buttalbakl@gmail.com>
2022-05-08 16:17:06 +0300

    Добавил миграцию для таблицы exchange_rate

--- migrations/m220508_130451_exchange_rate_table.php
+++ migrations/m220508_130451_exchange_rate_table.php
@@ -12,7 +12,15 @@ class m220508_130451_exchange_rate_table extends Migration
      */
     public function safeUp()
     {
-
+        $this->createTable('exchange_rate', [
+            'id' => $this->primaryKey(),
+            'exchange_id' => $this->integer()->notNull(),
+            'pair_id' => $this->integer()->notNull(),
+            'first_currency' => $this->string(15)->notNull(),
+            'second_currency' => $this->string(15)->notNull(),
+            'exchange_rate' => $this->double()->notNull(),
+            'created_at' => $this->timestamp()->notNull()->append('DEFAULT CURRENT_TIMESTAMP()'),
+        ]);
     }
 
     /**
@@ -20,9 +28,7 @@ class m220508_130451_exchange_rate_table extends Migration
      */
     public function safeDown()
     {
-        echo "m220508_130451_exchange_rate_table cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('exchange_rate');
     }
 
     /*
