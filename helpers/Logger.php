15fabaed22254c8022e649f6c58fba6ed08c7f58
Timur Makhmudov <buttalbakl@gmail.com>
2022-11-14 01:52:21 +0400

    Добавил новый url в записи ограничения логов

--- helpers/Logger.php
+++ helpers/Logger.php
@@ -7,10 +7,14 @@ class Logger
     public static function curlClient(array $params): void
     {
         $EXCEPT_URLS = [
-            'https://api.binance.com/api/v3/ticker/price'
+            'https://api.binance.com/api/v3/ticker/price',
+            'https://api.binance.com/sapi/v1/asset/tradeFee',
         ];
 
-        if (!in_array($params['url'], $EXCEPT_URLS)) {
+        $url = explode('?', $params['url']);
+        $url = rtrim($url[0], '/');
+
+        if (!in_array($url, $EXCEPT_URLS)) {
             \Yii::info("Запрос: {$params['url']}", 'curl-client');
             \Yii::info("Метод: {$params['method']}", 'curl-client');
             \Yii::info("Нагрузка: " . print_r($params['payload'], true), 'curl-client');
