fccf2877af8bdab0c1039ce54471d3533bea525c
Timur Makhmudov <buttalbakl@gmail.com>
2022-11-22 16:36:17 +0400

    Добавил валюдацию и описание полей в модель models/User

--- models/User.php
+++ models/User.php
@@ -10,4 +10,36 @@ class User extends ActiveRecord
     {
         return 'user';
     }
+
+    public function rules(): array
+    {
+        return [
+            [
+                [
+                    'email',
+                    'password_hash',
+                    'access_token',
+                ],
+                'string'
+            ],
+            [
+                [
+                    'created_at',
+                ],
+                'datetime',
+                'format' => 'php:Y-m-d H:i:s'
+            ],
+        ];
+    }
+
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'E-mail пользователя',
+            'password_hash' => 'Хеш пароля',
+            'access_token' => 'Токен доступа',
+            'created_at' => 'Временная метка создания',
+        ];
+    }
 }
