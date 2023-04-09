f225f37b61f04bcd96bbd43e530cfecd4f957a4a
Timur Makhmudov <buttalbakl@gmail.com>
2023-04-09 18:23:21 +0400

    Обновил способ парсинга данных по валютным парам с криптобиржи

--- commands/CurrencyPairUpdateController.php
+++ commands/CurrencyPairUpdateController.php
@@ -20,13 +20,11 @@ class CurrencyPairUpdateController extends Controller
     /**
      * @throws Exception
      */
-    public function actionIndex()
+    public function actionIndex(): void
     {
         $exchangeModels = Exchange::find()
             ->where(['is_disabled' => 0])
             ->all();
-
-        $exchangePairsList = [];
 
         foreach ($exchangeModels as $exchangeModel) {
             $EXCHANGE = \app\services\Exchange::getClass($exchangeModel->id);
@@ -37,58 +35,44 @@ class CurrencyPairUpdateController extends Controller
                 continue;
             }
 
-            $exchangePairsListFromDB = ExchangePair::find()
-                ->with('pair')
-                ->where([
+            foreach ($exchangePairsList as $exchangePair) {
+
+                $pairName = $exchangePair['first_currency'] . '/' . $exchangePair['second_currency'];
+                $pair = Pair::findOne(['name' => $pairName]);
+
+                if (!$pair) {
+                    $pair = Pair::add([
+                        'name' => $exchangePair['first_currency'] . '/' . $exchangePair['second_currency'],
+                        'first_currency' => $exchangePair['first_currency'],
+                        'second_currency' => $exchangePair['second_currency'],
+                    ]);
+                }
+
+                $exchangePairFromDB = ExchangePair::findOne([
                     'exchange_id' => $exchangeModel->id,
-                    'is_delisted' => false,
-                ])
-                ->all();
+                    'pair_id' => $pair->id,
+                ]);
 
-            foreach ($exchangePairsList as $exKey => $exchangePair) {
-                foreach ($exchangePairsListFromDB as $dbKey => $exchangePairFromDB) {
+                if (!$exchangePairFromDB) {
+                    $exchangePair['exchange_id'] = $exchangeModel->id;
+                    $exchangePair['pair_id'] = $pair->id;
 
-                    $pairName = $exchangePair['first_currency'] . '/' . $exchangePair['second_currency'];
-
-                    /* Если в БД уже есть валютная пара */
-                    if ($exchangePairFromDB->pair->name === $pairName) {
-
-                        $exchangePair['exchange_id'] = $exchangeModel->id;
-                        $exchangePair['pair_id'] = $exchangePairFromDB->pair->id;
-
-                        $exchangePairFromDB->load($exchangePair, '');
-                        $exchangePairFromDB->updated_at = null;
-                        $exchangePairFromDB->save();
-
-                        /* Удалим, чтобы узнать, каких пар нет в БД */
-                        unset($exchangePairsList[$exKey]);
-
-                        /* Удалим, чтобы знать, для каких валютных пар был произведен делистинг */
-                        unset($exchangePairsListFromDB[$dbKey]);
-
-                        continue 2;
-                    }
+                    ExchangePair::add($exchangePair);
+                } else {
+                    $exchangePairFromDB->load($exchangePair, '');
+                    $exchangePairFromDB->updated_at = null;
+                    $exchangePairFromDB->save();
                 }
             }
 
-            /* Запишем новые валютные пары */
-            foreach ($exchangePairsList as $exchangePair) {
-                $newPair = Pair::add([
-                    'name' => $exchangePair['first_currency'] . '/' . $exchangePair['second_currency'],
-                    'first_currency' => $exchangePair['first_currency'],
-                    'second_currency' => $exchangePair['second_currency'],
-                ]);
+            $exchangePairsListFromDB = ExchangePair::findAll(['exchange_id' => $exchangeModel->id]);
 
-                $exchangePair['exchange_id'] = $exchangeModel->id;
-                $exchangePair['pair_id'] = $newPair->id;
-
-                ExchangePair::add($exchangePair);
-            }
-
-            /* Если данные по валютной паре перестали поступать и не обновлялись больше полу года,
-                считаем, что биржа провела делистинг по этой паре */
+            /**
+             * Если данные по валютной паре перестали поступать и не обновлялись больше полу года,
+             * считаем, что биржа провела делистинг по этой паре
+             */
             foreach ($exchangePairsListFromDB as $exchangePairFromDB) {
-                if (strtotime($exchangePairFromDB->updated_at) < time() - 60*60*24*180) {
+                if (strtotime($exchangePairFromDB->updated_at) < time() - 60 * 60 * 24 * 180) {
                     $exchangePairFromDB->is_delisted = true;
                     $exchangePairFromDB->save();
                 }
